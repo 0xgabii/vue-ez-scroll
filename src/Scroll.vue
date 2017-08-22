05b4d9f21948339da8f7ee94b89ff238b2827286
Go Eun-Seok <gomonk3037@gmail.com>
2017-08-22 21:19:47 +0900

    Generate random id use component uid

--- src/Scroll.vue
+++ src/Scroll.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="vScroll">
-    
+  <div class="vScroll" :id="scrollId">
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Scroll',
+  props: {
+    horizontal: {
+      type: Boolean,
+      default: true,
+    },
+    vertical: {
+      type: Boolean,
+      default: true,
+    },
+    scrollAtOnce: {
+      type: Number,
+      default: 100,
+    },
+  },
+  data: () => ({
+    id: 0,
+  }),
+  computed: {
+    scrollId() {
+      return `scroll-${this.id}`;
+    },
+  },
+  mounted() {
+    // eslint-disable-next-line
+    this.id = this._uid;
+  },
 };
 </script>
