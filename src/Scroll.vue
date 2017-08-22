41f7afd82a4d607aea72fcb2496fc0c80f21242b
Go Eun-Seok <gomonk3037@gmail.com>
2017-08-22 23:21:48 +0900

    Get DOM infro from scroll component

--- src/Scroll.vue
+++ src/Scroll.vue
@@ -26,6 +26,15 @@ export default {
       vertical: 0,
       horizontal: 0,
     },
+
+    scrollEnv: {
+      scrollWidth: 0,
+      scrollHeight: 0,
+      clientWidth: 0,
+      clientHeight: 0,
+      maxScrollWidth: 0,
+      maxScrollHeight: 0,
+    },
   }),
   computed: {
     scrollId() {
@@ -41,6 +50,28 @@ export default {
 
       this.moveBar(delta);
     },
+    getComponentDOMInfo() {
+      const {
+        scrollWidth,
+        scrollHeight,
+        clientWidth,
+        clientHeight,
+      } = document.getElementById(this.scrollId);
+
+      this.scroll = {
+        vertical: 0,
+        horizontal: 0,
+      };
+
+      this.scrollEnv = {
+        scrollWidth,
+        scrollHeight,
+        clientWidth,
+        clientHeight,
+        maxScrollWidth: Math.max(0, scrollWidth - clientWidth),
+        maxScrollHeight: Math.max(0, scrollHeight - clientHeight),
+      };
+    },
   },
   mounted() {
     // eslint-disable-next-line
@@ -51,6 +82,8 @@ export default {
 
       component.addEventListener('mousewheel', this.handleScroll);
       component.addEventListener('DOMMouseScroll', this.handleScroll);
+
+      this.getComponentDOMInfo();
     });
   },
 };
